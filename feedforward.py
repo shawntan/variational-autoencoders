a96667e4cd07b2d087363d26b04942a5db2664bf
Shawn Tan <shawn@wtf.sg>
2015-11-25 00:45:46 +0800

    Refactored feedforward and vae code.

--- feedforward.py
+++ feedforward.py
@@ -3,37 +3,113 @@ import theano.tensor as T
 import numpy as np
 from theano_toolkit import utils as U
 from theano_toolkit.parameters import Parameters
+def initial_weights(input_size,output_size,factor=4):
+    return np.asarray(
+      np.random.uniform(
+         low  = -factor * np.sqrt(6. / (input_size + output_size)),
+         high =  factor * np.sqrt(6. / (input_size + output_size)),
+         size =  (input_size,output_size)
+      ),
+      dtype=theano.config.floatX
+    )
+
+def relu(X):
+    return ( X > 0 ) * X
+
+def relu_init(input_size,output_size):
+    return (np.random.randn(input_size,output_size) \
+         * np.sqrt(2.0 / input_size)).astype(np.float32)
+
+def build_classifier(
+        P, name,
+        input_sizes, hidden_sizes, output_size,
+        initial_weights=initial_weights,
+        output_initial_weights=lambda x,y: np.zeros(x,y),
+        activation=T.nnet.sigmoid,
+        output_activation=T.nnet.softmax):
+
+    combine_inputs = build_combine_transform(
+                        P,"%s_input"%name,
+                        input_sizes,hidden_sizes[0],
+                        initial_weights=initial_weights,
+                        activation=activation
+            )
+
+    transforms = build_stacked_transforms(P,name,hidden_sizes,
+            initial_weights=initial_weights,
+            activation=activation)
+
+    output = build_transform(
+        P,"%s_output"%name,hidden_sizes[-1],output_size,
+        initial_weights=output_initial_weights,
+        activation=output_activation)
+
+    def classify(Xs):
+        hidden_0 = combine_inputs(Xs)
+        hiddens = transforms(hidden_0)
+        return output(hiddens[-1])
+
+    return classify
 
 
-def build(P, name,
-          input_sizes, hidden_sizes, output_size,
-          activation=T.tanh):
+def build_stacked_transforms(
+        P,name,sizes,
+        activation,
+        initial_weights):
+    if len(sizes) == 1:
+        return lambda X:[X]
+    else:
+        transform_stack = build_stacked_transforms(
+                P,name,sizes[:-1],
+                initial_weights=initial_weights,
+                activation=activation)
+        transform = build_transform(
+                P,"%s_%d"%(name,len(sizes)-1),
+                sizes[-2],sizes[-1],
+                initial_weights=initial_weights,
+                activation=activation
+            )
+        def t(X):
+            layers = transform_stack(X)
+            return layers + [transform(layers[-1])]
+        return t
 
-    input_weights = []
-    for i, size in enumerate(input_sizes):
-        P["W_%s_hidden_input_%d" % (name, i)] = \
-            (0.1 * 2) * (np.random.rand(size, hidden_sizes[0]) - 0.5)
-        input_weights.append(P["W_%s_hidden_input_%d" % (name, i)])
-    P["b_%s_hidden_input" % name] = np.zeros((hidden_sizes[0],), dtype=np.float32)
-    b_hidden_input = P["b_%s_hidden_input" % name]
-    hidden_weights = []
-    prev_size = hidden_sizes[0]
-    for i, size in enumerate(hidden_sizes[1:]):
-        P["W_%s_hidden_%d" % (name, i)] = (0.1 * 2) * (np.random.rand(prev_size, size) - 0.5)
-        P["b_%s_hidden_%d" % (name, i)] = np.zeros((size,), dtype=np.float32)
-        prev_size = size
-        hidden_weights.append((P["W_%s_hidden_%d" % (name, i)], P["b_%s_hidden_%d" % (name, i)]))
-
-    P["W_%s_output" % name] = np.zeros((prev_size, output_size), dtype=np.float32)
-    P["b_%s_output" % name] = np.zeros((output_size,), dtype=np.float32)
-    W_output = P["W_%s_output" % name]
-    b_output = P["b_%s_output" % name]
-
-    def feedforward(X):
-        lin_hidden = sum(T.dot(x, input_weight) for x, input_weight in zip(X, input_weights))
-        hidden = activation(lin_hidden + b_hidden_input)
-        for W, b in hidden_weights:
-            hidden = activation(T.dot(hidden, W) + b)
-        output = T.dot(hidden, W_output) + b_output
+def build_transform(
+        P,name,input_size,output_size,
+        initial_weights,
+        activation):
+    P["W_%s"%name] = initial_weights(input_size,output_size)
+    P["b_%s"%name] = np.zeros((output_size,), dtype=np.float32)
+    W = P["W_%s"%name]
+    b = P["b_%s"%name]
+    def transform(X):
+        output = activation(T.dot(X, W) + b)
+        if hasattr(output,"name"): output.name = name
         return output
-    return feedforward
+    return transform
+
+def build_combine_transform(
+        P,name,input_sizes,output_size,
+        initial_weights,
+        activation):
+    weights = initial_weights(sum(input_sizes),output_size)
+
+    Ws = []
+    acc_size = 0
+    for i,size in enumerate(input_sizes):
+        P["W_%s_%d"%(name,i)] = weights[acc_size:acc_size+size]
+        Ws.append(P["W_%s_%d"%(name,i)])
+        acc_size += size
+    P["b_%s"%name] = np.zeros((output_size,), dtype=np.float32)
+    b = P["b_%s"%name]
+    def transform(Xs):
+        acc = 0.
+        for X,W in zip(Xs,Ws):
+            if X.dtype.startswith('int'):
+                acc += W[X]
+            else:
+                acc += T.dot(X,W)
+        output = activation(acc + b)
+        output.name = name
+        return output
+    return transform
--- vae.py
+++ vae.py
@@ -5,41 +5,99 @@ from theano_toolkit import utils as U
 
 import feedforward
 
-
 def build(P, name,
-          input_size,
-          encoder_hidden_sizes,
-          latent_size,
-          decoder_hidden_sizes=None,
-          activation=T.tanh):
+            input_size,
+            encoder_hidden_sizes,
+            latent_size,
+            decoder_hidden_sizes=None,
+            activation=T.nnet.softplus,
+            initial_weights=feedforward.relu_init):
 
     if decoder_hidden_sizes == None:
         decoder_hidden_sizes = encoder_hidden_sizes[::-1]
 
-    encoder = feedforward.build(P, "%s_encoder" % name,
-                                [input_size], encoder_hidden_sizes, latent_size * 2,
-                                activation=activation
-                                )
+    encode = build_inferer(P,"%s_encoder"%name,
+            [input_size],
+            encoder_hidden_sizes,
+            latent_size,
+            activation=activation,
+            initial_weights=initial_weights,
+            initialise_outputs=True
+        )
+    decode = build_inferer(P,"%s_decoder"%name,
+            [latent_size],
+            decoder_hidden_sizes,
+            input_size,
+            activation=activation,
+            initial_weights=initial_weights)
 
-    decoder = feedforward.build(P, "%s_decoder" % name,
-                                [latent_size], decoder_hidden_sizes, input_size,
-                                activation=activation
-                                )
+    def recon_error(X,encode=encode,decode=decode):
+        Z_latent, Z_mean, Z_logvar = encode([X])
+        _, recon_X_mean, recon_X_logvar = decode([Z_latent])
+        KL_d = kl_divergence(Z_mean,Z_logvar)
+        log_p_X = gaussian_log(recon_X_mean,recon_X_logvar,X)
+        cost = -(log_p_X - KL_d)
+        return recon_X_mean,T.mean(cost),T.mean(KL_d),T.mean(log_p_X)
+    return encode,decode,recon_error
 
-    def encode_decode(X):
-        mean_logvar = encoder([X])
-        mean = mean_logvar[:, :latent_size]
-        logvar = mean_logvar[:, latent_size:]
+def gaussian_log(mean,logvar,X):
+    return - 0.5 * T.sum(
+        np.log(2 * np.pi) + logvar +\
+                T.sqr(X - mean)/T.exp(logvar),axis=-1)
 
-        e = U.theano_rng.normal(size=logvar.shape)
-        latent = mean + e * T.exp(0.5 * logvar) # 0.5 * log std**2 = log std
+def kl_divergence(mean, logvar):
+    return -0.5 * T.sum(1 + logvar - T.sqr(mean) - T.exp(logvar), axis=-1)
 
-        recon_X = decoder([latent])
+def build_inferer(P,name,input_sizes,hidden_sizes,output_size,
+        initial_weights,activation,
+        initialise_outputs=False):
 
-        cost = -(
-            0.5 * T.sum(1 + logvar - mean**2 - T.exp(logvar), axis=1) -
-            0.5 * T.sum((recon_X - X)**2, axis=1)
+    combine_inputs = feedforward.build_combine_transform(
+            P,"%s_input"%name,
+            input_sizes,hidden_sizes[0],
+            initial_weights=initial_weights,
+            activation=activation
         )
 
-        return mean, logvar, recon_X, T.mean(cost)
-    return encode_decode
+    transform = feedforward.build_stacked_transforms(
+            P,name,hidden_sizes,
+            initial_weights=initial_weights,
+            activation=activation)
+
+    output = build_encoder_output(
+            P,name,
+            hidden_sizes[-1],output_size,
+            initialise_weights=(initial_weights if initialise_outputs else None)
+        )
+
+    def infer(Xs,samples=-1):
+        combine = combine_inputs(Xs)
+        hiddens = transform(combine)
+        latent, mean, logvar = output(hiddens[-1],samples=samples)
+        return latent, mean, logvar
+    return infer
+
+def build_encoder_output(P,name,input_size,output_size,initialise_weights=None):
+
+    if initialise_weights is None:
+        initialise_weights = lambda x,y:np.zeros((x,y))
+
+    P["W_%s_mean"%name] = 0.01 * np.random.randn(input_size,output_size)
+    P["b_%s_mean"%name] = np.zeros((output_size,))
+    P["W_%s_logvar"%name] = np.zeros((input_size,output_size))
+    P["b_%s_logvar"%name] = np.zeros((output_size,))
+    def output(X,samples=-1):
+        mean   = T.dot(X,P["W_%s_mean"%name]) + P["b_%s_mean"%name]
+        logvar = T.dot(X,P["W_%s_logvar"%name]) + P["b_%s_logvar"%name]
+        
+        std = T.exp(0.5 * logvar)
+        if samples == -1:
+            eps = U.theano_rng.normal(size=(logvar.shape[0],output_size))
+        else:
+            eps = U.theano_rng.normal(size=(logvar.shape[0],samples,output_size))
+            std = std.dimshuffle(0,'x',1)
+            mean = mean.dimshuffle(0,'x',1)
+
+        latent = mean + eps * std
+        return latent, mean, logvar
+    return output
